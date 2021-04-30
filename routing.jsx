105e11b8d8ab0deff302f9cc9c55bed92711ebe2
GrandWarlock7 <mcnair.screatorweek@gmail.com>
2021-04-29 22:55:07 -0500

    Added a useParams hook that hopefully works

---
+++ routing.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+function useParams(){
+const paramObject={};
+const searchParams=new URLSearchParams(window.location.search);
+searchParams.forEach((p, i) => {
+Object.defineProperty(paramObject, i, {
+    value: p,
+    writable:false
+})
+})
+return paramObject;
+}
